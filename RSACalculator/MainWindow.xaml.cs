e3f47a93ed3696b7a3acd2c8ec8187c8b7263148
kritikov <kritikov@yahoo.com>
2024-01-30 16:05:49 +0200

    working

--- RSACalculator/MainWindow.xaml.cs
+++ RSACalculator/MainWindow.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Security.Cryptography;
 using System.Numerics;
+using System.Reflection;
+using System.Collections;
 
 namespace RSACalculator
 {
@@ -38,29 +40,56 @@ namespace RSACalculator
                 using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
                 {
 
+                    var p = BigInteger.Parse("17");
+                    var q = BigInteger.Parse("19");
+                    var e = BigInteger.Parse("5");
+                    BigInteger n = p * q;
+                    BigInteger f_n = (p - 1) * (q - 1);
+                    BigInteger d = ModInverse(e, f_n);
+
+                    //var f = ModInverse(e, f_n);
+
+
+                    BigInteger m = 32;
+                    var bytes = Encoding.UTF8.GetBytes("Nick Kritikou");
+                    //var m = new BigInteger(bytes.Concat(new byte[] { 0 }).ToArray());
+                    var c = Encrypt(m, n, e);
+                    var m_ = Decrypt(c, d, n);
+
+
+                    var parameters = Create(p.ToByteArray(), q.ToByteArray(), e.ToByteArray(), n.ToByteArray());
+
                     //var parameters = new RSAParameters
                     //{
-                    //    Q = 17
+                    //    P = p.ToByteArray(),
+                    //    Q = q.ToByteArray(),
+                    //    D = d.ToByteArray(),
+                    //    Modulus = n.ToByteArray(),
+                    //    Exponent = e.ToByteArray(),
                     //};
 
-                    byte[] bytes = BitConverter.GetBytes(17);
+                    //var bytes = Encoding.UTF8.GetBytes(DataResource.RequiredModuleNotFound);
 
-                    var result = BigInteger.TryParse("76578687539749270334567433327419068016846482753554386520492865010545019207873571124020033190161215682615446396410129994711325892805940645283340607450605814292242214864807736616366963263577528008848595692050559543833562773099105599921926111989644313805176327043401089098178374013481826785604433013115807761169019821323105749924330624040032176017861852570855402207200857443405652874317191870029368327928342025351921450031345537488126081651667235334445880387925966636557003621257916083406394805099048054073995341365825171820787167114782405887040220243880710430205621846850135276025997937602400232787790214801902431955984378", null, out BigInteger value);
-                    value++;
+                    //byte[] bytes = BitConverter.GetBytes(17);
+
+                    //var result = BigInteger.TryParse("76578687539749270334567433327419068016846482753554386520492865010545019207873571124020033190161215682615446396410129994711325892805940645283340607450605814292242214864807736616366963263577528008848595692050559543833562773099105599921926111989644313805176327043401089098178374013481826785604433013115807761169019821323105749924330624040032176017861852570855402207200857443405652874317191870029368327928342025351921450031345537488126081651667235334445880387925966636557003621257916083406394805099048054073995341365825171820787167114782405887040220243880710430205621846850135276025997937602400232787790214801902431955984378", null, out BigInteger value);
+                    //value++;
 
 
                     //Pass the data to ENCRYPT, the public key information 
                     //(using RSACryptoServiceProvider.ExportParameters(false),
                     //and a boolean flag specifying no OAEP padding.
-                    encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+                    //encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+                    //encryptedData = RSAEncrypt(dataToEncrypt, parameters, false);
 
                     //Pass the data to DECRYPT, the private key information 
                     //(using RSACryptoServiceProvider.ExportParameters(true),
                     //and a boolean flag specifying no OAEP padding.
-                    decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+                    //decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+                    //decryptedData = RSADecrypt(encryptedData, parameters, false);
 
                     //Display the decrypted plaintext to the console. 
-                    Console.WriteLine("Decrypted plaintext: {0}", ByteConverter.GetString(decryptedData));
+                    //Console.WriteLine("Decrypted plaintext: {0}", ByteConverter.GetString(decryptedData));
                 }
             }
             catch (ArgumentNullException)
@@ -129,6 +158,107 @@ namespace RSACalculator
 
                 return null;
             }
+        }
+
+
+        private static RSAParameters Create(byte[] p, byte[] q, byte[] exponent, byte[] modulus)
+        {
+            var addlParameters = GetFullPrivateParameters(
+                p: new BigInteger(CopyAndReverse(p)),
+                q: new BigInteger(CopyAndReverse(q)),
+                e: new BigInteger(CopyAndReverse(exponent)),
+                modulus: new BigInteger(CopyAndReverse(modulus)));
+
+            return new RSAParameters
+            {
+                P = p,
+                Q = q,
+                Exponent = exponent,
+                Modulus = modulus,
+                D = addlParameters.D,
+                DP = addlParameters.DP,
+                DQ = addlParameters.DQ,
+                InverseQ = addlParameters.InverseQ,
+            };
+        }
+
+        private static RSAParameters GetFullPrivateParameters(BigInteger p, BigInteger q, BigInteger e, BigInteger modulus)
+        {
+            var n = p * q;
+            var phiOfN = (p - 1) * (q - 1);
+
+            var d = ModInverse(e, phiOfN);
+            //Assert.Equal(1, (d * e) % phiOfN);
+
+            var dp = d % (p - 1);
+            var dq = d % (q - 1);
+
+            var qInv = ModInverse(q, p);
+            //Assert.Equal(1, (qInv * q) % p);
+
+            return new RSAParameters
+            {
+                D = CopyAndReverse(d.ToByteArray()),
+                DP = CopyAndReverse(dp.ToByteArray()),
+                DQ = CopyAndReverse(dq.ToByteArray()),
+                InverseQ = CopyAndReverse(qInv.ToByteArray()),
+            };
+        }
+
+
+        /// <summary>
+        /// Calculates the modular multiplicative inverse of <paramref name="a"/> modulo <paramref name="m"/>
+        /// using the extended Euclidean algorithm.
+        /// </summary>
+        /// <remarks>
+        /// This implementation comes from the pseudocode defining the inverse(a, n) function at
+        /// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+        /// </remarks>
+        public static BigInteger ModInverse(BigInteger a, BigInteger n)
+        {
+            BigInteger t = 0, nt = 1, r = n, nr = a;
+
+            if (n < 0)
+            {
+                n = -n;
+            }
+
+            if (a < 0)
+            {
+                a = n - (-a % n);
+            }
+
+            while (nr != 0)
+            {
+                var quot = r / nr;
+
+                var tmp = nt; nt = t - quot * nt; t = tmp;
+                tmp = nr; nr = r - quot * nr; r = tmp;
+            }
+
+            if (r > 1) throw new ArgumentException(nameof(a) + " is not convertible.");
+            if (t < 0) t = t + n;
+            return t;
+        }
+
+        private static byte[] CopyAndReverse(byte[] data)
+        {
+            byte[] reversed = new byte[data.Length];
+            Array.Copy(data, 0, reversed, 0, data.Length);
+            Array.Reverse(reversed);
+            return reversed;
+        }
+
+
+
+        private static BigInteger Encrypt(BigInteger m, BigInteger n, BigInteger e)
+        {
+            return BigInteger.ModPow(m, e, n);
+        }
+
+        private static BigInteger Decrypt(BigInteger mEnc, BigInteger d, BigInteger n)
+        {
+            return BigInteger.ModPow(mEnc, d, n);
         }
     }
 }
