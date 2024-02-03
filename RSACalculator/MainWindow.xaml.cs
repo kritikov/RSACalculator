251ddd8b8e03bfbd769103f3376130ac946cbe26
Nick Kritikou <kritikov@yahoo.com>
2024-02-03 10:59:06 +0200

    working

--- RSACalculator/MainWindow.xaml.cs
+++ RSACalculator/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Security.Cryptography;
 using System.Numerics;
 using System.Reflection;
 using System.Collections;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace RSACalculator
 {
@@ -26,69 +27,9 @@ namespace RSACalculator
 
             try
             {
-                //Create a UnicodeEncoder to convert between byte array and string.
-                UnicodeEncoding ByteConverter = new UnicodeEncoding();
-
-                //Create byte arrays to hold original, encrypted, and decrypted data.
-                byte[] dataToEncrypt = ByteConverter.GetBytes("Data to Encrypt");
-                byte[] encryptedData;
-                byte[] decryptedData;
-
-                //Create a new instance of RSACryptoServiceProvider to generate
-                //public and private key data.
-                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
-                {
-
-                    var p = BigInteger.Parse("17");
-                    var q = BigInteger.Parse("19");
-                    var e = BigInteger.Parse("5");
-                    BigInteger n = p * q;
-                    BigInteger f_n = (p - 1) * (q - 1);
-                    BigInteger d = ModInverse(e, f_n);
-
-                    var number = StringToNumber("string");
-                    var text = NumberToString(number);
-
-                    BigInteger m = 32;
-
-                    //var m = new BigInteger(bytes.Concat(new byte[] { 0 }).ToArray());
-                    var c = Encrypt(m, n, e);
-                    var m_ = Decrypt(c, d, n);
-
-
-                    //var parameters = Create(p.ToByteArray(), q.ToByteArray(), e.ToByteArray(), n.ToByteArray());
-                    //var parameters = new RSAParameters
-                    //{
-                    //    P = p.ToByteArray(),
-                    //    Q = q.ToByteArray(),
-                    //    D = d.ToByteArray(),
-                    //    Modulus = n.ToByteArray(),
-                    //    Exponent = e.ToByteArray(),
-                    //};
-
-                    //var bytes = Encoding.UTF8.GetBytes(DataResource.RequiredModuleNotFound);
-
-                    //byte[] bytes = BitConverter.GetBytes(17);
-
-                    //var result = BigInteger.TryParse("76578687539749270334567433327419068016846482753554386520492865010545019207873571124020033190161215682615446396410129994711325892805940645283340607450605814292242214864807736616366963263577528008848595692050559543833562773099105599921926111989644313805176327043401089098178374013481826785604433013115807761169019821323105749924330624040032176017861852570855402207200857443405652874317191870029368327928342025351921450031345537488126081651667235334445880387925966636557003621257916083406394805099048054073995341365825171820787167114782405887040220243880710430205621846850135276025997937602400232787790214801902431955984378", null, out BigInteger value);
-                    //value++;
-
-
-                    //Pass the data to ENCRYPT, the public key information 
-                    //(using RSACryptoServiceProvider.ExportParameters(false),
-                    //and a boolean flag specifying no OAEP padding.
-                    //encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
-                    //encryptedData = RSAEncrypt(dataToEncrypt, parameters, false);
-
-                    //Pass the data to DECRYPT, the private key information 
-                    //(using RSACryptoServiceProvider.ExportParameters(true),
-                    //and a boolean flag specifying no OAEP padding.
-                    //decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
-                    //decryptedData = RSADecrypt(encryptedData, parameters, false);
-
-                    //Display the decrypted plaintext to the console. 
-                    //Console.WriteLine("Decrypted plaintext: {0}", ByteConverter.GetString(decryptedData));
-                }
+                //var text = TryDecrypt();
+                var text = TryFullDecrypt("string");
+                //TestRSA("string");
             }
             catch (ArgumentNullException)
             {
@@ -97,6 +38,80 @@ namespace RSACalculator
                 Console.WriteLine("Encryption failed.");
             }
 
+        }
+
+        /// <summary>
+        ///  try to decrypt the data of ergasia 3
+        /// </summary>
+        /// <returns></returns>
+        public static string TryDecrypt()
+        {
+            try
+            {
+                string nString = "84641628266040968134726266858994621642842114704041843715900121029779624800354442181024933538554572539172519633298583132480574427234887384864237862510536135711979678422472297405718744900411759436052738392404109005261750528594360602400820678382015390265136033037813004859176942808586218232493968301780230653856757977924165795192647059713220241170741461160395394356567033643278968175549740103490059728058303074181319743641739740188742499285217435763085176127055686256451491791548801388560202149363758975803673725819798718410301085155062206092370667133063831263112034304132340281237240943617970645337927088718346956199378919";
+                BigInteger n = BigInteger.Parse(nString);
+
+                string eString = "65537";
+                BigInteger e = BigInteger.Parse(eString);
+
+                string cString = "76578687539749270334567433327419068016846482753554386520492865010545019207873571124020033190161215682615446396410129994711325892805940645283340607450605814292242214864807736616366963263577528008848595692050559543833562773099105599921926111989644313805176327043401089098178374013481826785604433013115807761169019821323105749924330624040032176017861852570855402207200857443405652874317191870029368327928342025351921450031345537488126081651667235334445880387925966636557003621257916083406394805099048054073995341365825171820787167114782405887040220243880710430205621846850135276025997937602400232787790214801902431955984378";
+                BigInteger c = BigInteger.Parse(cString);
+
+                //var number = StringToNumber("string");
+                //var text = NumberToString(number);
+
+                BigInteger p = 3;
+                BigInteger q = n / p;
+                BigInteger f_n = (p - 1) * (q - 1);
+                BigInteger d = ModInverse(e, f_n);
+
+
+                var m = Decrypt(c, d, n);
+                var result = NumberToString(m);
+
+                return result;
+            }
+            catch (ArgumentNullException)
+            {
+                //Catch this exception in case the encryption did
+                //not succeed.
+                Console.WriteLine("routine failed.");
+                return "";
+            }
+        }
+
+        public static string TryFullDecrypt(string text)
+        {
+            try
+            {
+                // encryption
+                BigInteger m = StringToNumber(text);
+                
+                string nString = "84641628266040968134726266858994621642842114704041843715900121029779624800354442181024933538554572539172519633298583132480574427234887384864237862510536135711979678422472297405718744900411759436052738392404109005261750528594360602400820678382015390265136033037813004859176942808586218232493968301780230653856757977924165795192647059713220241170741461160395394356567033643278968175549740103490059728058303074181319743641739740188742499285217435763085176127055686256451491791548801388560202149363758975803673725819798718410301085155062206092370667133063831263112034304132340281237240943617970645337927088718346956199378919";
+                BigInteger q = BigInteger.Parse(nString);
+
+                string eString = "65537";
+                BigInteger e = BigInteger.Parse(eString);
+
+                BigInteger p = 3;
+                BigInteger n = p * q;
+                BigInteger f_n = (p - 1) * (q - 1);
+                BigInteger d = ModInverse(e, f_n);
+                var c = Encrypt(m, e, n);
+
+                // encryption
+                m = Decrypt(c, d, n);
+                var result = NumberToString(m);
+
+                return result;
+            }
+            catch (ArgumentNullException)
+            {
+                //Catch this exception in case the encryption did
+                //not succeed.
+                Console.WriteLine("routine failed.");
+                return "";
+            }
         }
 
         public static BigInteger StringToNumber(string text)
@@ -161,7 +176,52 @@ namespace RSACalculator
 
 
 
+        public static void TestRSA(string text)
+        {
+            try
+            {
+                //Create a UnicodeEncoder to convert between byte array and string.
+                UnicodeEncoding ByteConverter = new UnicodeEncoding();
 
+                //Create byte arrays to hold original, encrypted, and decrypted data.
+                byte[] dataToEncrypt = ByteConverter.GetBytes(text);
+                byte[] encryptedData;
+                byte[] decryptedData;
+
+                //Create a new instance of RSACryptoServiceProvider to generate
+                //public and private key data.
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+
+                    var p = BigInteger.Parse("17");
+                    var q = BigInteger.Parse("19");
+                    var e = BigInteger.Parse("5");
+                    BigInteger n = p * q;
+                    BigInteger f_n = (p - 1) * (q - 1);
+                    BigInteger d = ModInverse(e, f_n);
+
+                    //Pass the data to ENCRYPT, the public key information 
+                    //(using RSACryptoServiceProvider.ExportParameters(false),
+                    //and a boolean flag specifying no OAEP padding.
+                    encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+
+                    //Pass the data to DECRYPT, the private key information 
+                    //(using RSACryptoServiceProvider.ExportParameters(true),
+                    //and a boolean flag specifying no OAEP padding.
+                    decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+                    //decryptedData = RSADecrypt(encryptedData, parameters, false);
+
+                    //Display the decrypted plaintext to the console. 
+                    Console.WriteLine("Decrypted plaintext: {0}", ByteConverter.GetString(decryptedData));
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                //Catch this exception in case the encryption did
+                //not succeed.
+                Console.WriteLine("Encryption failed.");
+            }
+        }
         public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
         {
             try
@@ -274,8 +334,10 @@ namespace RSACalculator
             return reversed;
         }
 
-
-
        
+
+
+
+
     }
 }
